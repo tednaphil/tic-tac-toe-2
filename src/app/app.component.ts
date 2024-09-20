338e5f9516670205007d39539a92ef1ff09dbf4c
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-09-20 15:11:25 -0500

    Comment out inidividual players

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Player } from './player';
+import { BoardComponent } from './board/board.component';
+import { PlayerDisplayComponent } from './player-display/player-display.component';
+import { PlayersService } from './players.service';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BoardComponent, PlayerDisplayComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tic-tac-toe-2';
+
+  // player1: Player = {
+  //   id: 'One',
+  //   token: '🐝',
+  //   isTurn: true,
+  //   startGame: true,
+  //   moves: [],
+  //   wins: 0
+  // };
+
+  // player2: Player = {
+  //   id: 'Two',
+  //   token: '🐻',
+  //   isTurn: false,
+  //   startGame: false,
+  //   moves: [],
+  //   wins: 0
+  //   }
+
+  players: Player[] = [
+    {
+      id: 'One',
+      token: '🐝',
+      isTurn: true,
+      startGame: true,
+      moves: [],
+      wins: 0
+    },
+    {
+    id: 'Two',
+    token: '🐻',
+    isTurn: false,
+    startGame: false,
+    moves: [],
+    wins: 0
+    }
+  ];
+
+  // playersService: PlayersService = inject(PlayersService);
+  // players:  Player[] = [];
+  // player: Player = {};
+
+  constructor() {
+    // this.players = this.playersService.getPlayers();
+  }
 }
