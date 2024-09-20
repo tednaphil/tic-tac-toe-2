5e0b8e0e27c6133682725fe86859f4fe2b1f4b43
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-09-20 15:29:28 -0500

    Remove unused code

--- src/app/player-display/player-display.component.ts
+++ src/app/player-display/player-display.component.ts
@@ -1,23 +1,15 @@
 import { Component, inject, Input } from '@angular/core';
 import { PlayersService } from '../players.service';
 import { Player } from '../player';
-
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-player-display',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './player-display.component.html',
   styleUrl: './player-display.component.css'
 })
 export class PlayerDisplayComponent {
   @Input() player!: Player;
-  // playersService: PlayersService = inject(PlayersService);
-  // players:  Player[] = [];
-  // player: Player = {};
-
-
-  constructor() {
-    // this.players = this.playersService.getPlayers();
-  }
 
 }
